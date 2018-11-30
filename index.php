c57a7d21f5a417c83d096e69982e4eaa35aea835
nasser42 <nasser42100@live.fr>
2018-11-30 16:33:39 +0100

    2sd commit

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<link src="img/thermometer.jpg"></link>
+<link rel="stylesheet" type="text/css" href="main.css"></link>
 
 <h1>Température</h1>
 
-<?php 
+<?php
 
-echo "hello world";
+$file = "data.txt";
+$texte = file_get_contents($file);
+$data = json_decode($texte);
+
+?>
+<p>il fait <?php echo $data->temperature; ?>° avec <?php echo $data->humidite ?>% d'humidité</p>
+
+<div id="thermometer">
+  <div id="bargraph"></div>
+</div>
+
+
 
 
